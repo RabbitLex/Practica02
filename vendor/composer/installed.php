f9f1e71d6ce01b3414a1c91565187e9576881532
Misael Alexander Salazar Hernández <cite_13@hotmail.com>
2022-02-07 02:44:45 -0600

    Agregar libreria Composser

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '92cf40af647ab57c9466a347846d0470410f6ee0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '92cf40af647ab57c9466a347846d0470410f6ee0',
+            'dev_requirement' => false,
+        ),
+        'nubs/random-name-generator' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nubs/random-name-generator',
+            'aliases' => array(),
+            'reference' => '50abf24519badc5795a43c831f1d2a1faa45cb77',
+            'dev_requirement' => false,
+        ),
+    ),
+);
